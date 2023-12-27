c6569a194de98c47f5c3d7fbc963d54f31ac1b20
GovindRoG7 <112714370+GovindRoG7@users.noreply.github.com>
2023-12-27 23:43:21 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+
+
+document.getElementById("about-us-button").addEventListener("click", function aboutUs(){
+    document.getElementById("about").scrollIntoView({behavior:"smooth"});
+});
+
+
+document.getElementById("menu-button").addEventListener("click", function menu(){
+    document.getElementById("menu").scrollIntoView({behavior:"smooth"});
+});
+
+document.getElementById("contact-button").addEventListener("click", function contact(){
+    document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+});
+
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    content: ["./sushi.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
